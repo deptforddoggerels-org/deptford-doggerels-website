0a9683ae0fa2a5e442afd8657d740a9cc7ec19ec
jscanlo1 <deptforddoggerels@gmail.com>
2025-09-23 22:07:03 +0100

    Update poem slug

--- src/app/poems/[poemSlug]/page.tsx
+++ src/app/poems/[poemSlug]/page.tsx
@@ -2,48 +2,46 @@ import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
 import { notFound } from 'next/navigation';
-import Image from 'next/image';
-
-interface Params {
-  params: { poemSlug: string };
-}
+import { JSX } from 'react';
 
 type PoemData = {
   title: string;
   image: string;
   content: string;
   author: string;
-  date: string
+  date: string;
 };
 
-export default function PoemPage({ params }: Params) {
-  const { poemSlug } = params;
+interface PageProps {
+  params: {
+    poemSlug: string;
+  };
+}
 
-  const poem = getPoem(poemSlug);
+// You can keep it async, but use `Promise<JSX.Element>` as return type
+export default async function PoemPage({ params }: PageProps): Promise<JSX.Element> {
+  const { poemSlug } = params;
+  const poem = await getPoem(poemSlug);
 
   if (!poem) return notFound();
 
   return (
     <main className="px-6 pt-24 pb-8 w-full mx-auto bg-parchment-light bg-[url('/noise.png')]">
-
-      <div className='max-w-3xl mx-auto'>
+      <div className="max-w-3xl mx-auto">
         <h1 className="text-4xl font-pirata mb-4">{poem.title}</h1>
         <h5>by {poem.author}</h5>
         <article className="whitespace-pre-wrap leading-relaxed text-lg">
           {poem.content}
         </article>
-
       </div>
-
-
     </main>
   );
 }
 
-function getPoem(slug: string): PoemData | null {
+async function getPoem(slug: string): Promise<PoemData | null> {
   try {
     const filePath = path.join(process.cwd(), 'poems', `${slug}.md`);
-    const fileContent = fs.readFileSync(filePath, 'utf-8');
+    const fileContent = await fs.promises.readFile(filePath, 'utf-8');
     const { data, content } = matter(fileContent);
 
     return {
